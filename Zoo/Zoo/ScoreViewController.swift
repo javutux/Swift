e473320a2c24594a1cf626a58e0318e3997c6c62
Ieva Pipinyte <javutux@gmail.com>
2017-09-20 17:42:34 +0200

    scroll views

--- Zoo/Zoo/ScoreViewController.swift
+++ Zoo/Zoo/ScoreViewController.swift
@@ -11,11 +11,15 @@ import Foundation
 
 
 
-class ScoreViewController: UIViewController {
+class ScoreViewController: UIViewController, UIScrollViewDelegate {
     
     /// VARIABLES
+
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    var data:UITextView!
     
-   
+    
     
     ////Function
     override func viewDidLoad() {
@@ -23,7 +27,37 @@ class ScoreViewController: UIViewController {
         
         view.backgroundColor = UIColor.blue
         
+//        imageView = UIImageView(image: UIImage(named: "tuk.jpg"))
+//
+//        scrollView = UIScrollView(frame: view.bounds)
+//        scrollView.backgroundColor = UIColor.blue
+//        scrollView.contentSize = imageView.bounds.size
+//
+//        scrollView.contentOffset = CGPoint(x: 1000, y: 450)
+//        scrollView.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleWidth.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue)))
+//
+//        scrollView.addSubview(imageView)
+//        view.addSubview(scrollView)
+//
+//        setZoomScale()
+//    }
+//
+//
+//    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+//        return imageView
+//    }
+//
+//    func setZoomScale() {
+//        let imageViewSize = imageView.bounds.size
+//        let scrollViewSize = scrollView.bounds.size
+//        let widthScale = scrollViewSize.width / imageViewSize.width
+//        let heightScale = scrollViewSize.height / imageViewSize.height
+//
+//        scrollView.minimumZoomScale = min(widthScale, heightScale)
+//        scrollView.zoomScale = 1.0
     }
+
+    
     ////-----------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
