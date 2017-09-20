ad8a6342c1f8f7acf15d887b6f5243a014c33c05
Ieva Pipinyte <javutux@gmail.com>
2017-09-21 00:09:01 +0200

    small adj scroll view

--- Zoo/Zoo/ScoreViewController.swift
+++ Zoo/Zoo/ScoreViewController.swift
@@ -27,34 +27,6 @@ class ScoreViewController: UIViewController, UIScrollViewDelegate {
         
         view.backgroundColor = UIColor.blue
         
-//        imageView = UIImageView(image: UIImage(named: "tuk.jpg"))
-//
-//        scrollView = UIScrollView(frame: view.bounds)
-//        scrollView.backgroundColor = UIColor.blue
-//        scrollView.contentSize = imageView.bounds.size
-//
-//        scrollView.contentOffset = CGPoint(x: 1000, y: 450)
-//        scrollView.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleWidth.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue)))
-//
-//        scrollView.addSubview(imageView)
-//        view.addSubview(scrollView)
-//
-//        setZoomScale()
-//    }
-//
-//
-//    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-//        return imageView
-//    }
-//
-//    func setZoomScale() {
-//        let imageViewSize = imageView.bounds.size
-//        let scrollViewSize = scrollView.bounds.size
-//        let widthScale = scrollViewSize.width / imageViewSize.width
-//        let heightScale = scrollViewSize.height / imageViewSize.height
-//
-//        scrollView.minimumZoomScale = min(widthScale, heightScale)
-//        scrollView.zoomScale = 1.0
     }
 
     
